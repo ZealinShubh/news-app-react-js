c968c59f7b759338308a1c8d501ed19381eb416c
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-07-01 12:22:25 +0530

    Fetching news from a static array done

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  articles = [
+    {
+        "source":{"id":"news-com-au","name":"News.com.au"},"author":"Andrew McMurtry","title":"‘Extraordinary’ scenes stun cricket world","description":"England wicketkeeper Jonny Bairstow has taken security into his own hands, dragging a protester from the Lord&rsquo;s playing surface in wild scenes.","url":"https://www.news.com.au/sport/cricket/extraordinary-scenes-as-englands-jonny-bairstow-manhandles-protester/news-story/d0f9b7d6deaadea9dcee07022c19d613","urlToImage":"https://content.api.news/v3/images/bin/49408b488f1ce28f27de3fef771376b0","publishedAt":"2023-06-28T11:10:00Z","content":"England wicketkeeper Jonny Bairstow has taken security into his own hands, dragging a protester from the Lord’s playing surface in wild scenes.\r\nThe first day of the second Ashes Test was stopped aft… [+2198 chars]"
+    },
+    {
+        "source":{"id":"espn-cric-info","name":"ESPN Cric Info"},"author":null,"title":"PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com","description":"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com","url":"http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket","urlToImage":"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg","publishedAt":"2020-04-27T11:41:47Z","content":"Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    },
+    {
+        "source":{"id":"espn-cric-info","name":"ESPN Cric Info"},"author":null,"title":"What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com","description":"Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com","url":"http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again","urlToImage":"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg","publishedAt":"2020-03-30T15:26:05Z","content":"Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+}
+]
+  constructor(){
+    super();
+    console.log("I am a constructor from News Component");
+    this.state={
+      articles: this.articles,
+      loading: false
+    }
+  }
+
   render() {
     return (
       <div className='container my-3'>
         <h2>FactFinder Top Headlines</h2>
         <div className="row">
-            <div className="col-md-4">
-                <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-            <div className="col-md-4">
-                <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-            <div className="col-md-4">
-                <NewsItem title="myTitle" description="myDescription"/>
-            </div>
+        {this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url}>
+            <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} imageurl={element.urlToImage} newsurl={element.url}/>
+        </div>
+        })}
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageurl, newsurl} = this.props;
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-        <img src="https://content.api.news/v3/images/bin/49408b488f1ce28f27de3fef771376b0" className="card-img-top" alt="..." />
+        <img src={imageurl} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a href={newsurl} target='_blank' className="btn btn-primary btn-sm">Read More</a>
             </div>
         </div>
       </div>

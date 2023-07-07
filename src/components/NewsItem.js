367d972d0227a7d09c668b0e678ebdf1898d60b4
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-07-07 12:25:51 +0530

    Fetching news from api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,12 +5,12 @@ export class NewsItem extends Component {
   render() {
     let {title, description, imageurl, newsurl} = this.props;
     return (
-      <div>
+      <div className='my-4'>
         <div className="card" style={{width: "18rem"}}>
-        <img src={imageurl} className="card-img-top" alt="..." />
+        <img src={!imageurl?"https://i.gadgets360cdn.com/large/galaxy_unpacked_2023_samsung_1688609323941.jpg":imageurl} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{title}...</h5>
-                <p className="card-text">{description}...</p>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
                 <a href={newsurl} target='_blank' className="btn btn-primary btn-sm">Read More</a>
             </div>
         </div>

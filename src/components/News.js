dad4321bafa18e5db1c53d0eb7f786bced1b12d8
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-07-07 13:06:06 +0530

    Added Next & Previous Btn

--- src/components/News.js
+++ src/components/News.js
@@ -7,20 +7,49 @@ export class News extends Component {
     super();
     this.state={
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
   }
 
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e2ffabb5f60b442d9800d17afdbf7251";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e2ffabb5f60b442d9800d17afdbf7251&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+  }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e2ffabb5f60b442d9800d17afdbf7251&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+  })
+}
+
+  handleNextClick = async () => {
+    console.log("Next");
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e2ffabb5f60b442d9800d17afdbf7251&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      })
+    } 
   }
 
   render() {
-    console.log("render");
     return (
       <div className='container my-3'>
         <h1>FactFinder Top Headlines</h1>
@@ -30,6 +59,10 @@ export class News extends Component {
             <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageurl={element.urlToImage} newsurl={element.url}/>
         </div>
         })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href={newsurl} target='_blank' className="btn btn-primary btn-sm">Read More</a>
+                <a rel="noreferrer" href={newsurl} target='_blank' className="btn btn-dark btn-sm">Read More</a>
             </div>
         </div>
       </div>

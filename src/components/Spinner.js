d31e2563d2d0ad448e56aa3b70e9d5a85adbc32b
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-08-25 00:28:45 +0530

    API Key Hidden

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -5,7 +5,7 @@ export class Spinner extends Component {
   render() {
     return (
       <div className='text-center'>
-        <img className='my-3' src={loading} alt="loading" />
+        <img src={loading} alt="loading" />
       </div>
     )
   }

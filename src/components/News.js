5edc82c97a0dfe33aac1a780a4262ef4c38d5d59
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-08-24 23:42:04 +0530

    Added Top Loading Bar

--- src/components/News.js
+++ src/components/News.js
@@ -33,15 +33,19 @@ export class News extends Component {
   }
 
   async updateNews(){
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e2ffabb5f60b442d9800d17afdbf7251&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(60);
     console.log(parsedData);
     this.setState({articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false
     })
+    this.props.setProgress(100);
   }
 
   async componentDidMount(){

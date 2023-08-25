6817b9fe0b4e905ce7abb06ea518239be9bd5d92
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-08-25 20:25:09 +0530

    Refactoring code to function based

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Link} from "react-router-dom";
 
-export class Navbar extends Component {
+const Navbar = () =>{
 
-  render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -28,7 +27,6 @@ export class Navbar extends Component {
         </nav>
       </div>
     )
-  }
 }
 
 export default Navbar
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
+const NewsItem = (props)=> {
 
-  render() {
-    let {title, description, imageurl, newsurl, author, date} = this.props;
+    let {title, description, imageurl, newsurl, author, date} = props;
     return (
       <div className='my-4'>
         <div className="card">
@@ -17,7 +16,6 @@ export class NewsItem extends Component {
         </div>
       </div>
     )
-  }
 }
 
 export default NewsItem
